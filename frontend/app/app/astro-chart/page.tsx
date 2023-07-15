5efa7f85aa04f5ee66a8d2f4a03587417b867038
IncioMan <alex.incerti@outlook.com>
2023-07-15 17:28:43 +0200

    server side

--- frontend/app/app/astro-chart/page.tsx
+++ frontend/app/app/astro-chart/page.tsx
@@ -3,26 +3,51 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
 import ClientSideAstroChartPage from './ClientSideAstroChartPage';
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { ITraderSummary } from "@/interfaces/Interfaces";
 
-import top_today_sellers from '../../data/top_today_sellers.json';
-import top_today_buyers from '../../data/top_today_buyers.json';
-import top_weekly_sellers from '../../data/top_weekly_sellers.json';
-import top_weekly_buyers from '../../data/top_weekly_buyers.json';
-import top_monthly_sellers from '../../data/top_monthly_sellers.json';
-import top_monthly_buyers from '../../data/top_monthly_buyers.json';
+const client = new S3Client({
+  region: 'eu-west-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ? process.env.AWS_ACCESS_KEY_ID : '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ? process.env.AWS_SECRET_ACCESS_KEY : '',
+  }
+})
 
-export default function Home() {
+async function loadData(filename: string) {
+  const command = new GetObjectCommand({
+    Bucket: 'incioman-data-analysis',
+    Key: `astro_trading/20230715/${filename}`,
+  });
+
+  try {
+    const response = await client.send(command);
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    const str = await response?.Body?.transformToString();
+    if (str) return JSON.parse(str)
+  } catch (err) {
+    console.error(err);
+  }
+};
+export default async function Home() {
+  const top_today_buyers: ITraderSummary[] | undefined = await loadData("top_today_buyers.json");
+  const top_weekly_buyers: ITraderSummary[] | undefined = await loadData("top_weekly_buyers.json");
+  const top_monthly_buyers: ITraderSummary[] | undefined = await loadData("top_monthly_buyers.json");
+  const top_today_sellers: ITraderSummary[] | undefined = await loadData("top_today_sellers.json");
+  const top_weekly_sellers: ITraderSummary[] | undefined = await loadData("top_weekly_sellers.json");
+  const top_monthly_sellers: ITraderSummary[] | undefined = await loadData("top_monthly_sellers.json");
+
 
   return (
     <div style={{ background: 'linear-gradient(to bottom, #041339, #37609f)' }}
       className="h-screen">
       <ClientSideAstroChartPage
-        top_today_sellers={top_today_sellers}
-        top_today_buyers={top_today_buyers}
-        top_weekly_sellers={top_weekly_sellers}
-        top_weekly_buyers={top_weekly_buyers}
-        top_monthly_sellers={top_monthly_sellers}
-        top_monthly_buyers={top_monthly_buyers}
+        top_today_sellers={top_today_sellers ? top_today_sellers : []}
+        top_today_buyers={top_today_buyers ? top_today_buyers : []}
+        top_weekly_sellers={top_weekly_sellers ? top_weekly_sellers : []}
+        top_weekly_buyers={top_weekly_buyers ? top_weekly_buyers : []}
+        top_monthly_sellers={top_monthly_sellers ? top_monthly_sellers : []}
+        top_monthly_buyers={top_monthly_buyers ? top_monthly_buyers : []}
       />
     </div>
   )
