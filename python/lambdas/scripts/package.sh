60361b0a115cbc39a0fee84c4345e4c38a28e63c
IncioMan <alex.incerti@outlook.com>
2023-07-22 14:42:17 +0200

    ...

--- python/lambdas/scripts/package.sh
+++ python/lambdas/scripts/package.sh
@@ -2,6 +2,6 @@ pipenv requirements > requirements.txt
 rm -r deps/
 pip install -r requirements.txt --target deps/
 cd deps
-zip -r ../my-deployment-package.zip .
+zip -r ../lambda.zip .
 cd ..
-zip my-deployment-package.zip lambda_function.py
+zip lambda.zip lambda_function.py
