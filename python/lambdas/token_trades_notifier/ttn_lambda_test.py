8ecbbcc661773af105e946d2c01c146f08abc58c
IncioMan <alex.incerti@outlook.com>
2023-07-22 09:38:59 +0200

    fix names

--- python/lambdas/token_trades_notifier/ttn_lambda_test.py
+++ python/lambdas/token_trades_notifier/ttn_lambda_test.py
@@ -4,11 +4,11 @@ import json
 
 @pytest.mark.parametrize(
     ("message_file"), [
-    pytest.param('messages/token_trades_notifier/buy_astro.json',id="Buy ASTRO"),
-    pytest.param('messages/token_trades_notifier/sell_astro.json',id="Sell ASTRO"),
-    pytest.param('messages/token_trades_notifier/sell_kuji.json',id="Sell KUJI"),
-    pytest.param('messages/token_trades_notifier/buy_mars.json',id="Buy MARS"),
-    pytest.param('messages/token_trades_notifier/buy_strd.json',id="Buy STRD")
+    pytest.param('messages/token_trades_notifier/2_buy_astro.json',id="Buy ASTRO"),
+    pytest.param('messages/token_trades_notifier/1_sell_astro.json',id="Sell ASTRO"),
+    pytest.param('messages/token_trades_notifier/3_sell_kuji.json',id="Sell KUJI"),
+    pytest.param('messages/token_trades_notifier/5_buy_mars.json',id="Buy MARS"),
+    pytest.param('messages/token_trades_notifier/7_buy_strd.json',id="Buy STRD")
 ])
 def test_lambda_function(message_file):
     with open(message_file, 'r') as file:
